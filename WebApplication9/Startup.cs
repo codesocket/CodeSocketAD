18e2768b5268d62ba12ebaaca01d62d566cb951e
Abdul Rehman <rehman.a@live.com>
2016-02-17 16:31:51 -0500

    Initial

---
+++ WebApplication9/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace WebApplication9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
