da277c8b6fb014e08948d18eda68579b5f6a39bd
Abdul Rehman <rehman.a@live.com>
2016-02-18 17:03:00 -0500

    Added WebJob

---
+++ CodeSocket-WebJob/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSocket_WebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.TraceError("Something went wrong in this WebJob");
+        }
+    }
+}
